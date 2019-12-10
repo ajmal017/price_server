fb69d47ee1ed8719a127cdd25d5ac5e6df7042aa
Ashwin Kapur <ashwin.kapur@gmail.com>
2019-12-10 16:02:56 -0500

    EOD

--- src/app/app.py
+++ src/app/app.py
@@ -12,12 +12,25 @@ Main file which assembles all routers into one.
 from fastapi import FastAPI
 
 from .routers import basic_timeseries
+from starlette.middleware.cors import CORSMiddleware
 
+origins = [
+    "http://localhost:3000",
+    "http://localhost:8080"
+]
 
 app = FastAPI(
     title="Price Server",
     description="Provides a REST API to Norgate Data",
     version="1.0.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(basic_timeseries.router)
 
